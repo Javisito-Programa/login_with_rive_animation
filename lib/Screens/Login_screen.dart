5e09f2fc965f2325db05a29f62a56e694045ecd5
Javier Morales <le24081092@merida.tecnm.mx>
2026-02-10 14:34:43 -0600

    feat(rive):animated_login_bear

--- lib/Screens/Login_screen.dart
+++ lib/Screens/Login_screen.dart
@@ -1,5 +1,3 @@
-//Autor:Itran Javier Morales Broca
-
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -11,14 +9,65 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //control para mostrar u ocultar la contraseña
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
+    //para obtener el tamaño de la pantalla y usarlo para ajustar el diseño
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: RiveAnimation.asset("animated_login_bear.riv")),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('assets/animated_login_bear.riv'),
+              ),
+              //Para separacion
+              const SizedBox(height: 10),
+              TextField(
+                //Para mostrar un tipo de tecleado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    //Para redondear los bordes del campo de texto
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock), //Cerrado o Seguro
+                  suffixIcon: IconButton(
+                    //if terniario
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      //Refresca el widget para mostrar u ocultar la contraseña
+                      setState(() {
+                        //Cambiar el estado de _obscureText para mostrar u ocultar la contraseña
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
